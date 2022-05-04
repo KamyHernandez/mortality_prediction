94ecd43c2d55166b8b671322032f7e25091f8ded
Gio Barbosa <casanovag@uninorte.edu.co>
2022-05-04 14:56:34 -0500

    Credential update for database connection

--- src/dbConection.R
+++ src/dbConection.R
@@ -1,17 +1,13 @@
 # connection to MIMIC-server
 
+#install.packages("RPostgres") # Install package
 library(DBI)
+connection <- dbConnect(RPostgres::Postgres(),
+                 dbname = 'mimic', # name db for all the groups
+                 host = 'mimic-server.postgres.database.azure.com',
+                 port = 5432, # port standard of SMDB
+                 user = 'postgres', # user for all the groups
+                 password = 'securepass321.') # user for all the groups
 
-connection = dbConnect(
-  odbc::odbc(),
-  Driver   = 'PostgreSQL Unicode',
-  Server   = 'mimic-server.postgres.database.azure.com',
-  Database = 'mimic',
-  UID      = 'postgres', 
-  PWD      = '*******', 
-  Port     = 5432,
-  sslmode  = 'require'
-)
-
-# list all tables from database on mimiciii schema
-dbGetQuery(connection, "SELECT table_name FROM information_schema.tables WHERE table_schema = 'mimiciii';")
+dbListTables(connection) # show list tables in db
+dbGetQuery(connection, "SELECT table_name FROM information_schema.tables;")
